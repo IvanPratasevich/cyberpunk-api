e022623b332f3d22ec8528e6147cb1af532d0dc4
IvanPratasevich <iv.pratasevich@gmail.com>
2023-04-07 00:00:19 +0300

    feat: update bootstrap

--- cyberpunk-api/src/main.ts
+++ cyberpunk-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+const PORT = process.env.PORT || 4444;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: NestExpressApplication =
+    await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+
+  await app.listen(PORT, () => {
+    console.log(`Start server on the ${PORT} port!`);
+  });
 }
+
 bootstrap();
